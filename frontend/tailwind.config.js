423ae11f5e973a9588683b228e56a26e584d6232
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2024-12-15 01:13:46 +0530

    convert require to import

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
@@ -8,7 +10,6 @@ export default {
     extend: {},
   },
   plugins: [
-    import('daisyui'),
+    daisyui,
   ],
-}
-
+};
