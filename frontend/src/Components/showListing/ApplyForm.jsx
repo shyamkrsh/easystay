48824dafc35787e96c960a78062d4b3a3ee6f2d3
Shyam Kumar Sharma <136264959+shyamkrsh@users.noreply.github.com>
2025-04-15 23:48:24 +0530

    Update ApplyForm.jsx

--- frontend/src/Components/showListing/ApplyForm.jsx
+++ frontend/src/Components/showListing/ApplyForm.jsx
@@ -37,7 +37,7 @@ function ApplyForm({ id, amount , title}) {
         async function handlePaymentVerify(data){
             try {
                 const options = {
-                    key: import.meta.env.RAZORPAY_KEY_ID,
+                    key: import.meta.env.VITE_RAZORPAY_KEY_ID,
                     amount: data.amount,
                     currency: "INR",
                     name: title,
