4c8f5eb143697f10c3f86b801566e0956088343f
Shyam Kumar Sharma <sharmashyamkumar717@gmail.com>
2025-04-16 00:03:32 +0530

    Bugs fixed

--- backend/index.js
+++ backend/index.js
@@ -43,5 +43,7 @@ app.use("/api/application", applicationRouter);
 app.use("/api/contact", contactRouter);
 
 
-
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}
+)
